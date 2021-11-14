All three angles extracted. fix angel 4 sign

src/vision_1.py
@@ -16,8 +16,17 @@ import message_filters
 class joint_estimation_1:
     def __init__(self):
         rospy.init_node('vision_1', anonymous=True)
+
+        self.link_lengths = {
+            "link_1": 4.0,
+            "link_2": 0.0,
+            "link_3": 3.2,
+            "link_4": 2.8           
+        }
+
         self.image_sub1 = message_filters.Subscriber('/camera1/robot/image_raw', Image)
         self.image_sub2 = message_filters.Subscriber('/camera2/robot/image_raw', Image)
+        self.joint_angles_pub = rospy.Publisher('joint_states_1', Float64MultiArray, queue_size=10)
         self.ts = message_filters.TimeSynchronizer([self.image_sub1, self.image_sub2], 100)
         self.ts.registerCallback(self.callback)
         self.bridge = CvBridge()
@@ -29,10 +38,15 @@ class joint_estimation_1:
         except CvBridgeError as e:
             print(e)
         
-        cv2.imshow('YZ Image', self.yz_image)
-        cv2.imshow('XZ Image', self.xz_image)
-        self.detect_joint_angles(self.yz_image, self.xz_image)
+        # cv2.imshow('YZ Image', self.yz_image)
+        # cv2.imshow('XZ Image', self.xz_image)
+
+        
         cv2.waitKey(1)
+        self.joints = Float64MultiArray()
+        x = self.detect_joint_angles(self.yz_image, self.xz_image)
+        self.joints.data = x
+        self.joint_angles_pub.publish(self.joints)
     
     # Calculate the conversion from pixel to meter
     def pixel2meter(self,image):
@@ -43,10 +57,12 @@ class joint_estimation_1:
         dist = np.sum((circle1Pos - circle2Pos)**2)
         return 4 / np.sqrt(dist)
     
+
       # In this method you can focus on detecting the centre of the red circle
     def detect_red(self,yz_image, xz_image):
         # Isolate the blue colour in the image as a binary image
-        yz_mask = cv2.inRange(yz_image, (0, 0, 100), (0, 0, 255))
+        yz_mask = cv2.inRange(yz_image, (0, 0, 1), (0, 0, 255))
+        cv2.imshow('ass', 255 - self.yz_image[:,:,0])
         # This applies a dilate that makes the binary region larger (the more iterations the larger it becomes)
         kernel = np.ones((5, 5), np.uint8)
         yz_mask = cv2.dilate(yz_mask, kernel, iterations=3)
@@ -161,27 +177,57 @@ class joint_estimation_1:
     def detect_joint_angles(self, yz_image, xz_image):
         a = self.pixel2meter(yz_image)
         # Obtain the centre of each coloured blob 
-        center = a * self.detect_green(yz_image, xz_image)
-        circle1Pos = a * self.detect_yellow(yz_image, xz_image) 
-        circle2Pos = a * self.detect_blue(yz_image, xz_image) 
-        circle3Pos = a * self.detect_red(yz_image, xz_image)
+        circle1Pos = a * self.detect_green(yz_image, xz_image)
+        circle2Pos = a * self.detect_yellow(yz_image, xz_image) 
+        circle3Pos = a * self.detect_blue(yz_image, xz_image) 
+        circle4Pos = a * self.detect_red(yz_image, xz_image)
 
-        vector_center_circle1 = (center - circle1Pos)
-        vector_circle1_circle2 = (circle1Pos - circle2Pos)
-        vector_circle2_circle3 = (circle2Pos - circle3Pos)
+        vector_circle1_circle2 = (circle2Pos - circle1Pos)
+        vector_circle2_circle3 = (circle3Pos - circle2Pos)
+        vector_circle3_circle4 = (circle4Pos - circle3Pos)
+
+        print('=== Circle Positions ===\nCircle1Pos: {}\nCircle2Pos: {}\nCircle3Pos: {}\nCircle4Pos: {}\n=== ==='.format(
+            circle1Pos, circle2Pos, circle3Pos, circle4Pos))
+
+        print('=== Vector Positions ===\nCircle1 to Circle2: {}\nCircle2 to Circle3: {}\nCircle3 to Circle4: {}\n===   ==='.format(
+            vector_circle1_circle2, vector_circle2_circle3, vector_circle3_circle4))
+
+        # ===== for drawing detecting blob centers on image ====
+
+        circle1Pos_img = self.detect_green(yz_image, xz_image)
+        circle2Pos_img = self.detect_yellow(yz_image, xz_image) 
+        circle3Pos_img = self.detect_blue(yz_image, xz_image) 
+        circle4Pos_img = self.detect_red(yz_image, xz_image)
+
+        image_with_centers = cv2.circle(self.xz_image, (circle1Pos_img[0], circle1Pos_img[2]), 2, (255, 255, 255), cv2.FILLED)
+        image_with_centers = cv2.circle(image_with_centers, (circle2Pos_img[0], circle2Pos_img[2]), 2, (255, 255, 255), cv2.FILLED)
+        image_with_centers = cv2.circle(image_with_centers, (circle3Pos_img[0], circle3Pos_img[2]), 2, (255, 255, 255), cv2.FILLED)
+        image_with_centers = cv2.circle(image_with_centers, (circle4Pos_img[0], circle4Pos_img[2]), 2, (255, 255, 255), cv2.FILLED)
+
+        cv2.imshow('Images with blob centers', image_with_centers)
+
+        # ===== Angles =====
+
+        print('=== ANGLES ===')
+        ja2 = - np.arctan2(circle2Pos[0] - circle3Pos[0], circle2Pos[2] - circle3Pos[2])
+        ja3 = np.arctan2(circle2Pos[1] - circle3Pos[1], circle2Pos[2] - circle3Pos[2])
+        unit_vector1 = vector_circle2_circle3 / np.linalg.norm(vector_circle2_circle3)
+        unit_vector2 = vector_circle3_circle4 / np.linalg.norm(vector_circle3_circle4)
+        dot_1_2 = np.dot(unit_vector1, unit_vector2)
+        ja4 = np.arccos(dot_1_2)
+
+        print([ja2, ja3, ja4])
+        print("\n")
 
         # theta2 = np.arctan2(vector_circle1_circle2[0], vector_circle1_circle2[2])
-        # print("Link1 1: {}".format(circle1Pos - center))
-        # print("Link 3: {}".format(circle2Pos - circle1Pos))
-        # print("Link 4: {}".format(circle3Pos - circle2Pos))
 
-        print(np.arctan(np.array(vector_circle1_circle2[0], vector_circle1_circle2[2])))
-        print(np.arctan(np.array(vector_circle1_circle2[1], vector_circle1_circle2[2])))
+        # print(np.arctan(np.array(vector_circle1_circle2[0], vector_circle1_circle2[2])))
+        # print(np.arctan(np.array(vector_circle1_circle2[1], vector_circle1_circle2[2])))
 
-        print(np.arccos(np.array(np.dot(vector_circle1_circle2, np.array([0, 1, 0]) / ()))))
+        # print(np.arccos(np.array(np.dot(vector_circle1_circle2, np.array([0, 1, 0]) / ()))))
 
-        # print(circle1Pos)
-        # print(circle2Pos)
+        return np.array([ja2, ja3, ja4])
+    
 # call the class
 def main(args):
     joint_estimation = joint_estimation_1()